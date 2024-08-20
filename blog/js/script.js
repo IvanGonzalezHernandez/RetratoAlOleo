741a534c0749adf231491cad0dce883f463747a8
IvanGonzalezHernandez <ivangonzalez.code@gmail.com>
2024-08-20 17:39:14 +0200

    Ultimando detalles

--- blog/js/script.js
+++
@@ -1,6 +0,0 @@
-// Blog Scripts
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Blog cargado y listo para interactuar');
-
-    // Paginación, carga diferida de imágenes, etc.
-});
